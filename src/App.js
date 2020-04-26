ada67a1df9a5e2342324546983074d243c93e345
junpay <junpoko625@gmail.com>
2020-04-26 15:37:51 +0900

    modify App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div><h1>Hello world</h1></div>
+      <div><h1>hello world</h1></div>
     );
   }
 }
+
 
 export default App;
