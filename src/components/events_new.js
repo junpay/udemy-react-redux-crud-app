abac6619b9b89d7ec59f3cfcb40320d927298b35
junpay <junpoko625@gmail.com>
2020-05-09 12:25:44 +0900

    apply material-ui to events_new and events_show components

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 import { postEvent } from '../actions'
 
@@ -12,15 +14,23 @@ class EventsNew extends Component {
   }
   // 引数fieldには入力された値が渡ってくる
   renderField(field) {
-    console.log(field);
+    console.log("field.input", field.meta.error);
     const { input, label, type, meta: { touched, error }} = field
     
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {/* バリデーションの表示 */}
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField 
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
+      // <div>
+      //   <input {...input} placeholder={label} type={type} />
+      //   {/* バリデーションの表示 */}
+      //   {touched && error && <span>{error}</span>}
+      // </div>
     )
   }
 
@@ -32,17 +42,13 @@ class EventsNew extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props;
+    const style = { margin: 12 }
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
-        <div>
-          <Field label="Title" name="title" type="text" component={this.renderField} />
-          <Field label="Body" name="body" type="text" component={this.renderField} />
-        </div>
-
-        <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-          <Link to="/" >Cancel</Link>
-        </div>
+        <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
+        <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+        <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+        <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />} />
       </form>
     )
   }
